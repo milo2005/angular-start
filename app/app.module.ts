b7aa9237d7cf6aad523c92ae55aea2563ed2cc69
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-09-04 18:16:16 -0500

    app.module fixed

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule}   from '@angular/forms';
 import {HttpModule}    from '@angular/http';
 
-import {routing} from "./app.routes.ts";
+import {routing} from "./app.routes";
 import {AppComponent} from "./app.component";
 
 @NgModule({
